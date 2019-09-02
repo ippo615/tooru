760f852df8c9463741d3d4204778148fad15ba78
Andrew Ippoliti <acippolit@gmail.com>
2019-09-02 16:46:41 -0400

    Added ability to save/load board to an ASCII string

--- js/script.js
+++ js/script.js
@@ -194,7 +194,99 @@ class BoardAnalyzer {
 		return b2;
 	}
 }
+class BoardRenderSimpleString extends BoardAnalyzer {
+	constructor( board ){
+		super( board );
+		
+		this.players = {
+			'none': 0b00000,
+			'player1': 0b00100,
+			'player2': 0b01000,
+			'player3': 0b01100,
+			'player4': 0b10000
+		};
+		
+		this.directions = {
+			0  : 0b00000,
+			90 : 0b00001,
+			180: 0b00010,
+			270: 0b00011
+		};
+		
+		this.characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	}
+	
+	asSimpleString(){
+		// First convert all directions to 0,1,2,3 (0,90,180,270)
+		// Then convert all players to 0,1,2,3,4 (None,PlayerA,PlayerB,PlayerC,PlayerD)
+		// Each piece can then be encoded as 3 bits player + 2 bits direction
+		// for a 10x10 board that's 500 bits = ~64 bytes
+		
+		// To make it simpler let's just build everything as 8 bits
+		// and limit it to specific characters
+		// 5 bits = 2**5 == 32 (ALPHABET+6)
+		// 6 bits = 2**6 == 64 (UPPER+lower+8)
+		
+		// We dont want to waste data on width and height
+		// so instead we put a ',' at the end of the first row
+		// Then we can calculate width and height
+		let result = '';
+		for( let y=0, h=this.height; y<h; y+=1 ){
+			for( let x=0, w=this.width; x<w; x+=1 ){
+				let piece = this.board.getPieceAt(x,y);
+				let value = 0;
+				value += this.directions[ piece.direction % 360 ];
+				value += this.players[ piece.player ];
+				result += this.characters[ value ];
+			}
+		}
+		result = result.slice(0, this.width) + "," + result.slice(this.width);
+		
+		return result;
+	}
+	
+	_getPlayer( characterIndex ){
+		let player = 'none';
+		let that = this;
+		Object.keys(this.players).forEach(function (key) {
+			let p = that.players[key];
+			if( (p & characterIndex) == p ){
+				player = key;
+			}
+		});
+		return player;
+	}
+	_getDirection( characterIndex ){
+		let direction = 0;
+		let that = this;
+		Object.keys(this.directions).forEach(function (key) {
+			let d = that.directions[key];
+			if( (d & characterIndex) == d ){
+				direction = parseInt(key,10);
+			}
+		});
+		return direction;
+	}
+	fromSimpleString( data ){
+		let pieceCount = data.length-1;
+		let width = data.indexOf(',');
+		let height = pieceCount / width;
+		let simpleString = data.replace(',','');
+		let board = new Board( width, height );
+		board.generate();
+		for( let y=0; y<height; y+=1 ){
+			for( let x=0; x<height; x+=1 ){
+				let piece = board.getPieceAt(x,y);
+				let c = simpleString[y*width+x];
+				let characterIndex = this.characters.indexOf( c );
+				piece.player = this._getPlayer( characterIndex );
+				piece.direction = this._getDirection( characterIndex );
+			}
+		}
 
+		return board;
+	}
+}
 class BoardHtmlRender extends BoardAnalyzer {
 	constructor( board ){
 		super( board );
@@ -303,6 +395,10 @@ $(function(){
 	let boarderRender = new BoardHtmlRender(b);
 	$('#game').html( boarderRender.asHtmlString() );
 	$('#style-holder').html( '<style>'+boarderRender.generateCss()+'</style>' );
+
+	let boardString = (new BoardRenderSimpleString(b)).asSimpleString();
+	let boardFromString = (new BoardRenderSimpleString(b)).fromSimpleString(boardString);
+	console.info( boardFromString );
 
 	var b2 = new BoardAnalyzerChainSize(b);
 	console.info( b2.data );
